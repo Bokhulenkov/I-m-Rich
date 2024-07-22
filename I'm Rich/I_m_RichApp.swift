d7fc7578fbf9e571ad7818fabca3868ba229b04b
Bokhulenkov <bokhulenkov@gmail.com>
2024-07-22 11:41:43 +0300

    Initial Commit

---
+++ I'm Rich/I_m_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_m_RichApp.swift
+//  I'm Rich
+//
+//  Created by Alexander Bokhulenkov on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct I_m_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
